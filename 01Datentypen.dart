255d36b319d8e261bd47b195b10bced637bea340
littlelion308 <55046277+littlelion308@users.noreply.github.com>
2020-07-13 14:43:35 +0200

    Create 01Datentypen.dart

---
+++ 01Datentypen.dart
@@ -0,0 +1,11 @@
+void main() {
+  int INTA = 1;
+  int INTB = 2;
+  print(INTA);print(INTB);
+  double DOUBA = 1.0;
+  double DOUBB = 2.0;
+print(DOUBA);
+print(DOUBB);
+  String STRA = "hfjkasl";
+  print(STRA);
+}
