533078d25d4f721ed182ebce8ab91ccda552e5cb
littlelion308 <55046277+littlelion308@users.noreply.github.com>
2020-07-14 10:33:06 +0200

    Create 02_Stringverarbeitung.dart

---
+++ 02_Stringverarbeitung.dart
@@ -0,0 +1,8 @@
+void main() {
+  String meinName = "Christoph";
+  print("Mein Name ist $meinName");
+  double doub = 1.369;
+  String stdou = doub.toString();
+  print(stdou);
+  print(meinName.length);
+}
