ede36bf2684c41c37732276bbbefb51bb54d802d
littlelion308 <55046277+littlelion308@users.noreply.github.com>
2020-07-14 11:17:40 +0200

    Create 04_If_Else_Strukturen.dart

---
+++ 04_If_Else_Strukturen.dart
@@ -0,0 +1,9 @@
+void main(){
+  int zahlengrenze = 1;
+  if(zahlengrenze >= 10){
+    print("Zahlengrenze>=10");
+  }else{
+    print("Zahlengrenze<10");
+  }
+  print(zahlengrenze>=10?"Zahlengrenze in ternaer >= 10":"Zahlengrenze in tenaer < 10");
+}
